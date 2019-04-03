2f52fd5dd0aabedb2ea631d37b27cbe0f2462b66
mikesuchor <mikesuchor@gmail.com>
2019-04-03 00:31:15 -0600

    Removed Header Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Header from '../Header/Header'
 import Dashboard from '../Dashboard/Dashboard'
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <Header />
         <Dashboard />
       </div>
     );
