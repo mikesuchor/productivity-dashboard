fcdf650fc1283d2216de2844477b54b036b81316
mikesuchor <mikesuchor@gmail.com>
2019-04-03 00:30:56 -0600

    Added some api calls, work in progress

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -6,14 +6,39 @@ class Weather extends React.Component {
     super(props)
     
     this.state = {
-      
+      latitude: 0,
+      longitude: 0,
+      weather: {}
     }
+  }
+
+  componentDidMount() {
+    if (window.navigator.geolocation) { 
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.getWeatherData(position.coords.latitude, position.coords.longitude)
+      })
+    }
+  }
+
+  getWeatherData(latitude, longitude) {
+    const weatherapi = "https://fcc-weather-api.glitch.me/api/current?"
+
+    fetch(`${weatherapi}lat=${latitude}&lon=${longitude}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          latitude,
+          longitude,
+          weather: data
+        })
+      })
   }
   
   render() {
     return (
       <div className="weather">
         <h3>Weather</h3>
+        {console.log(this.state.weather.main.temp)}
       </div>
     );
   }
