86dcf3dfa07e6e4782123b65fbebf58b96ec78fa
mikesuchor <mikesuchor@gmail.com>
2019-04-29 23:07:03 -0600

    Renamed header to navbar component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Dashboard from '../Dashboard/Dashboard'
+import Navbar from '../Navbar/Navbar'
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
+        <Navbar />
         <Dashboard />
       </div>
     );
