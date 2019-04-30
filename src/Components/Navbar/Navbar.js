5ebc2896b5a6df73954a14ed5df69cd1a9a69f27
mikesuchor <mikesuchor@gmail.com>
2019-04-29 23:06:42 -0600

    Renamed header to navbar component

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div id="navbar">
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navbar
