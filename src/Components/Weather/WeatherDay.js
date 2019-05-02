2ab317e7eec7a873c34fe73976c547cbf7057077
mikesuchor <mikesuchor@gmail.com>
2019-05-02 02:11:09 -0600

    Added weather icons, max temp and min temp

--- src/Components/Weather/WeatherDay.js
+++ src/Components/Weather/WeatherDay.js
@@ -3,8 +3,40 @@ import './WeatherDay.css'
 
 class WeatherDay extends React.Component {
     render() {
+        const weatherIcon = () => {
+            switch(this.props.weather.daily.data[this.props.day].icon) {
+                case "clear-day":
+                    return("wi wi-day-sunny")
+                case "clear-night":
+                    return("wi wi-night-clear")
+                case "partly-cloudy-day":
+                    return("wi wi-day-cloudy")
+                case "partly cloudy-night":
+                    return("wi wi-night-alt-cloudy")
+                case "cloudy":
+                    return("wi wi-cloudy")
+                case "rain":
+                    return("wi wi-rain")
+                case "sleet":
+                    return("wi wi-sleet")
+                case "snow":
+                    return("wi wi-snow")
+                case "wind":
+                    return("wi wi-cloudy-gusts")
+                case "fog":
+                    return("wi wi-fog")
+                default:
+                    return("wi wi-na")
+            }
+        }
+
         return(
-            <div>{this.props.day}</div>
+            <div className="weather_day">
+                {console.log(this.props.weather)}
+                <i className={weatherIcon()}></i>
+                <p class="temperatures temphigh">{Math.round(this.props.weather.daily.data[this.props.day].temperatureHigh)}&deg;C</p>
+                <p class="temperatures templow">{Math.round(this.props.weather.daily.data[this.props.day].temperatureLow)}&deg;C</p>
+            </div>
         )
     }
     
